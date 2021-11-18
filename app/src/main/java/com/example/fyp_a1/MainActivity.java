0ab4068bc86f156e0f2bf4ada2d8828162106a2f
bilal <bilal_4m@live.com>
2021-11-18 21:00:49 +0500

    changes

--- app/src/main/java/com/example/fyp_a1/MainActivity.java
+++ app/src/main/java/com/example/fyp_a1/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    Button btn,btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.signup_btn1);
+        btn1 = (Button) findViewById(R.id.login_btn1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),HomeScreenActivity.class);
                 startActivity(intent);
             }
         });
